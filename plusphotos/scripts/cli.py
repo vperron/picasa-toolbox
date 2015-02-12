47c610717586eceba5acf531732d0a898dbe3956
Victor Perron <victor@iso3103.net>
2015-02-12 13:33:45 +0100

    Add debug option, command support

--- plusphotos/scripts/cli.py
+++ plusphotos/scripts/cli.py
@@ -3,17 +3,27 @@
 import click
 
 from plusphotos import inspect_dir
+from plusphotos.settings import Settings
 
 
-@click.command('plusphotos')
+@click.group()
+@click.option('--debug/--no-debug', default=False)
+@click.pass_context
+def cli(ctx, debug):
+    if not ctx.obj:
+        ctx.obj = Settings()
+    ctx.obj.DEBUG = debug
+
+
+@cli.command()
 @click.argument('path')
-@click.option('--good', is_flag=True, default=False)
-@click.option('--bad', is_flag=True, default=False)
-def cli(path, good, bad):
+@click.pass_context
+@click.option('--good', is_flag=True, help='Only list images bearing valid tags.')
+@click.option('--bad', is_flag=True, help='Only list images not bearing tags.')
+def list(ctx, path, good, bad):
     for img in inspect_dir(path):
         if good and not bad and not img.has_exif:
             continue
-
         if bad and not good and img.has_exif:
             continue
 
---
+++ plusphotos/settings.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+
+class Settings(object):
+
+    def __init__(self):
+        pass

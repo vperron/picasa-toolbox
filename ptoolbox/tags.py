66b20aa7fbd322a051ab7ae26ecd8c46f7605763
Victor Perron <victor@iso3103.net>
2015-02-20 07:14:45 +0100

    Remove orientation tag parsing, not needed.

--- ptoolbox/tags.py
+++ ptoolbox/tags.py
@@ -5,21 +5,6 @@ from datetime import datetime
 TAG_WIDTH = 'EXIF ExifImageWidth'
 TAG_HEIGHT = 'EXIF ExifImageLength'
 TAG_DATETIME = 'Image DateTime'
-TAG_ORIENTATION = 'Image Orientation'
-
-# XXX: this is a terrible way to retrieve the orientations. Exifread regretfully does not
-# get back raw EXIF orientations, and no other library is available on pip as of today.
-ORIENTATIONS = [
-    'Horizontal (normal)',
-    'Mirrored horizontal',
-    'Rotated 180',
-    'Mirrored vertical',
-    'Mirrored horizontal then rotated 90 CCW',
-    'Rotated 90 CCW',
-    'Mirrored horizontal then rotated 90 CW',
-    'Rotated 90 CW',
-]
-
 
 def parse_time(tags):
     tag = tags.get(TAG_DATETIME, None)
@@ -40,10 +25,3 @@ def parse_height(tags):
     if not tag:
         raise KeyError(TAG_HEIGHT)
     return int(str(tag), 10)
-
-
-def parse_orientation(tags):
-    tag = tags.get(TAG_ORIENTATION, None)
-    if not tag:
-        raise KeyError(TAG_ORIENTATION)
-    return ORIENTATIONS.index(str(tag)) + 1  # XXX: convert back to original EXIF orientation

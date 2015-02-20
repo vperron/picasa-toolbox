3b5b3afbc66f60df45f0458ffdd0d37b9a7c50d0
Victor Perron <victor@iso3103.net>
2015-02-20 08:17:59 +0100

    Add homemade fast width/height reader for JPEG files

--- ptoolbox/tags.py
+++ ptoolbox/tags.py
@@ -1,10 +1,28 @@
 # -*- coding: utf-8 -*-
 
+import struct
 from datetime import datetime
 
 TAG_WIDTH = 'EXIF ExifImageWidth'
 TAG_HEIGHT = 'EXIF ExifImageLength'
 TAG_DATETIME = 'Image DateTime'
+
+
+def jpeg_size(path):
+    """Get image size.
+    Structure of JPEG file is:
+        ffd8 [ffXX SSSS DD DD ...] [ffYY SSSS DDDD ...]  (S is 16bit size, D the data)
+    We look for the SOF0 header 0xffc0; its structure is
+       [ffc0 SSSS PPHH HHWW ...] where PP is 8bit precision, HHHH 16bit height, WWWW width
+    """
+    with open(path, 'rb') as f:
+        _, header_type, size = struct.unpack('>HHH', f.read(6))
+        while header_type != 0xffc0:
+            f.seek(size - 2, 1)
+            header_type, size = struct.unpack('>HH', f.read(4))
+        bpi, height, width = struct.unpack('>BHH', f.read(5))
+    return width, height
+
 
 def parse_time(tags):
     tag = tags.get(TAG_DATETIME, None)
